0a6c11ceb45ff1cce07ea63b09898be3085174a7
buerkle <rjefts@gmail.com>
2012-12-12 16:52:42 -0600

    Fix help command to not print out null

--- java/src/main/java/com/rfcode/zm/examples/Main.java
+++ java/src/main/java/com/rfcode/zm/examples/Main.java
@@ -9,9 +9,9 @@ package com.rfcode.zm.examples;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -133,12 +133,12 @@ public class Main {
 
         @Override()
         public void run() {
-            String name = null;
-            if (!_parameters.isEmpty()) {
-                name = _parameters.get(0);
+            if (_parameters.isEmpty()) {
+                _jc.usage();
             }
-
-            _jc.usage(name);
+            else {
+                _jc.usage(_parameters.get(0));
+            }
         }
     }
 }
